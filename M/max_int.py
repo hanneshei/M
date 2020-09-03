4f5d6c844264fe2acf3303f84fc9bcd98f8a1df9
hanneshei <70704132+hanneshei@users.noreply.github.com>
2020-09-03 15:15:38 +0000

    Update max_int.py

--- M/max_int.py
+++ M/max_int.py
@@ -1,5 +1,18 @@
+'''
+This program finds the largest number by user input.
+The user inputs a number until a negative number is entered.
+'''
+
 num_int = int(input("Input a number: "))    # Do not change this line
+max_int = num_int + 1
 
-print("The maximum is", max_int)    # Do not change this line
+for i in range(0, max_int):
+
+    num_int = int(input("Input a number: "))
+
+    if num_int >= 0:
+        continue
 
 
+    if num_int < 0:
+        print("The maximum is", max_int)    # Do not change this line
